38897a8cb9c9e410deb0f327a7f89f3f64e00f18
Ethan Passard <e.passardplprs.fr>
2025-01-10 23:29:02 +0100

    feat(modification/ajout) : ajout de nouveau jeux et de nouvelle fonctionnalité

--- src/Main.java
+++ src/Main.java
@@ -12,21 +12,31 @@ public class Main {
         nom = scan.nextLine();
 
         sautLigne();
-        System.out.println("Bonjour à toi " + nom + ", Bienvenue dans mon projet !\n"+
-                "tu te trouve la dans un projet qui regroupe la plupart de mes projet que j'ai eu a faire.\n"+
-                "Ici tu retrouvera des programmes pour jouer, calculer, plein de type de programme en gros.\n"+
-                "Prends le temps d'explorer, et n'oublie pas de tout lire pour ne rien louper.\n"+
+        System.out.println("Bonjour à toi " + nom + ", Bienvenue dans mon projet !\n" +
+                "tu te trouve la dans un projet qui regroupe la plupart de mes projet que j'ai eu a faire.\n" +
+                "Ici tu retrouvera des programmes pour jouer, calculer, plein de type de programme en gros.\n" +
+                "Prends le temps d'explorer, et n'oublie pas de tout lire pour ne rien louper.\n" +
                 "Bonne découverte !");
 
         System.out.println("Veux tu joués a un jeux ? :");
-        question = scan.nextLine();
+        question = scan.nextLine().toUpperCase();;
 
-        if (question.equals("oui")) {
-            System.out.println("Liste des Jeux disponible :\n"+
-                    "1 - bataille navalle\n"+
-                    "2 - master mind\n");
-        }else{
+        if (question.equals("OUI")) {
+            System.out.println("Liste des Jeux disponible :\n" +
+                    "1 - bataille navalle\n" +
+                    "2 - master mind\n"+
+                    "3 - pendu\n"+
+                    "4 - juste prix\n"+
+                    "5 - morpion\n"+
+                    "6 - xxx\n"+
+                    "7 - xxx\n"+
+                    "8 - xxx\n"+
+                    "9 - xxx\n");
+
+        } else {
             System.out.println("tant pis");
+            scan.close();
+            return;
         }
 
         System.out.println("A quel jeux veux tu jouer ? :");
@@ -42,16 +52,20 @@ public class Main {
                 masterMind();
                 break;
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("Lancement du jeux du pendu\n");
+                pendu();
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("Lancement du jeux juste prix\n");
+                justePrix();
                 break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("Lancement du jeux morpion\n");
+                morpion();
                 break;
             case 6:
-                System.out.println("Saturday");
+                System.out.println("Lancement Calcul de moyenne\n");
+                calculMoyenne();
                 break;
             case 7:
                 System.out.println("Sunday");
@@ -381,7 +395,280 @@ public class Main {
         System.out.println("Merci d'avoir joué au MasterMind. À bientôt !");
         scanner.close();
 
+
+
     }
+
+    //Fonction Jeux Pendu
+    public static void pendu() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Étape 1 : Joueur 1 entre le mot à deviner
+        System.out.print("Joueur 1, entrez un mot à deviner : ");
+        String motADeviner = scanner.nextLine().toUpperCase();
+        System.out.print("Confirmez-vous le mot '" + motADeviner + "' ? (oui/non) : ");
+        String confirmation = scanner.nextLine().toLowerCase();
+
+        while (!confirmation.equals("oui")) {
+            System.out.print("Entrez un nouveau mot : ");
+            motADeviner = scanner.nextLine().toUpperCase();
+            System.out.print("Confirmez-vous le mot '" + motADeviner + "' ? (oui/non) : ");
+            confirmation = scanner.nextLine().toLowerCase();
+        }
+
+        char[] motCache = new char[motADeviner.length()];
+        for (int i = 0; i < motCache.length; i++) {
+            motCache[i] = '_';
+        }
+
+        int essaisRestants = 9;
+        boolean motTrouve = false;
+        String lettresProposees = "";
+
+        // Étape 2 : Joueur 2 essaie de deviner le mot
+        while (essaisRestants > 0 && !motTrouve) {
+            System.out.println("\nMot à deviner (" + essaisRestants + " essais restants) : " + new String(motCache));
+            afficherPendu(9 - essaisRestants); // Affiche le dessin du pendu
+            System.out.print("1 - proposer une lettre");
+            System.out.print("\n2 - proposer un mot");
+            System.out.print("\nJoueur 2, à votre tour, vous voulez proposer une lettre ou un mot (1/2): ");
+            int choix = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choix == 1) {
+                System.out.print("Proposez une lettre : ");
+                char lettre = scanner.nextLine().toUpperCase().charAt(0);
+
+                if (lettresProposees.indexOf(lettre) != -1) {
+                    System.out.println("Cette lettre a déjà été proposée.");
+                    continue;
+                }
+
+                lettresProposees += lettre;
+                boolean lettreTrouvee = false;
+                for (int i = 0; i < motADeviner.length(); i++) {
+                    if (motADeviner.charAt(i) == lettre) {
+                        motCache[i] = lettre;
+                        lettreTrouvee = true;
+                    }
+                }
+
+                if (!lettreTrouvee) {
+                    essaisRestants--;
+                }
+            } else if (choix == 2) {
+                System.out.print("Proposez un mot : ");
+                String motPropose = scanner.nextLine().toUpperCase();
+
+                if (motPropose.equals(motADeviner)) {
+                    motTrouve = true;
+                    break;
+                } else {
+                    System.out.println("Le mot proposé est incorrect.");
+                    essaisRestants--;
+                }
+            } else {
+                System.out.println("Choix invalide.");
+            }
+
+            motTrouve = new String(motCache).equals(motADeviner);
+        }
+
+        // Fin de la partie
+        if (motTrouve) {
+            System.out.println("\nFélicitations ! Joueur 2 a trouvé le mot : " + motADeviner);
+        } else {
+            afficherPendu(9);
+            System.out.println("\nDommage ! Le mot à deviner était : " + motADeviner);
+        }
+
+        // Relancer une nouvelle partie
+        System.out.print("Voulez-vous relancer une partie ? (oui/non) : ");
+        String relancer = scanner.nextLine().toLowerCase();
+        if (relancer.equals("oui")) {
+            //System.out.println("ok");
+            pendu();
+        } else {
+            System.out.println("C'est la fin du jeu, j'espère que tu as passés un bon moment !");
+            System.out.println("Tu peux revenir jouer dès que tu le veux.");
+            System.out.println("A très vite ! :)");
+        }
+
+        scanner.close();
+    }
+    // Méthode pour afficher le dessin du pendu
+    public static void afficherPendu(int etape) {
+        switch (etape) {
+            case 1:
+                System.out.println("      ");
+                System.out.println("      ");
+                System.out.println("       ");
+                System.out.println("       ");
+                System.out.println("    ===");
+                break;
+            case 2:
+                System.out.println("     +");
+                System.out.println("     |");
+                System.out.println("     |");
+                System.out.println("     |");
+                System.out.println("    ===");
+                break;
+            case 3:
+                System.out.println(" +---+");
+                System.out.println("     |");
+                System.out.println("     |");
+                System.out.println("     |");
+                System.out.println("    ===");
+                break;
+            case 4:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println("     |");
+                System.out.println("     |");
+                System.out.println("    ---");
+                break;
+            case 5:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println(" |   |");
+                System.out.println("     |");
+                System.out.println("    ===");
+                break;
+            case 6:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println("/|   |");
+                System.out.println("     |");
+                System.out.println("    ===");
+                break;
+            case 7:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println("/|\\  |");
+                System.out.println("     |");
+                System.out.println("    ===");
+                break;
+            case 8:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println("/|\\  |");
+                System.out.println("/    |");
+                System.out.println("    ===");
+                break;
+            case 9:
+                System.out.println(" +---+");
+                System.out.println(" O   |");
+                System.out.println("/|\\  |");
+                System.out.println("/ \\  |");
+                System.out.println("    ===");
+                break;
+        }
+    }
+
+    //Fonction Jeux Juste prix
+    public static void justePrix(){
+
+        System.out.println("Jeux du juste prix en cour de developpement");
+    }
+
+    //Fonction Jeux Morpion
+    public static void morpion(){
+
+        System.out.println("Jeux du morpion en cour de developpement");
+    }
+
+    //Fonction Calcul de moyenne
+    public static void calculMoyenne(){
+
+        Scanner scanner = new Scanner(System.in);
+
+//variable
+        int nombreEleve;
+        int nombreNote;
+
+
+//Code
+        System.out.println("Bonjour, combien d'élève y a t-il ?");
+        nombreEleve = scanner.nextInt();
+        String[] nomEtudiant = new String[nombreEleve];
+        double [] moyenne = new double[nombreEleve];
+        System.out.println("d'accord il y aura "+nombreEleve+" élèves");
+        System.out.println("Maintenant combien de notes auront ces élèves ?");
+        nombreNote = scanner.nextInt();
+        double [][] note = new double[nombreEleve][nombreNote];
+        double [][] coef = new double[nombreEleve][nombreNote];
+        System.out.println("donc il y aura "+nombreEleve+" élèves et "+nombreNote+" notes par élèves.");
+
+        for (int i = 0; i < nombreEleve; i++) {
+            System.out.println("Entrez le nom de l'élève n°" + (i + 1) + " :");
+            scanner.skip("\n");
+            nomEtudiant[i] = scanner.nextLine();
+
+            double sommeNote = 0;
+            double sommeCoef = 0;
+
+            System.out.println("Entrez les notes et coefficients de " + nomEtudiant[i] + " :");
+            for (int j = 0; j < nombreNote; j++) {
+                System.out.println("Entrez la note " + (j + 1) + " :");
+                note[i][j] = scanner.nextDouble();
+                System.out.println("Entrez le coefficient de la note " + (j + 1) + " :");
+                coef[i][j] = scanner.nextDouble();
+
+                sommeNote += note[i][j] * coef[i][j];
+                sommeCoef += coef[i][j];
+            }
+
+            moyenne[i] = sommeNote / sommeCoef;
+        }
+
+        for (int i = 0; i < nombreEleve; i++) {
+            System.out.println("Elève : " + nomEtudiant[i]);
+            for (int j = 0; j < nombreNote; j++) {
+                System.out.println("Note " + (j + 1) + " - " + note[i][j] + " / Coef - " + coef[i][j]);
+            }
+            System.out.println("Moyenne : " + moyenne[i]);
+            System.out.println();
+        }
+
+        for (int i = 0; i < nombreEleve - 1; i++) {
+            for (int j = i + 1; j < nombreEleve; j++) {
+                if (moyenne[i] < moyenne[j]) {
+
+                    String echangeNom = nomEtudiant[i];
+                    nomEtudiant[i] = nomEtudiant[j];
+                    nomEtudiant[j] = echangeNom;
+
+                    double echangeMoyenne = moyenne[i];
+                    moyenne[i] = moyenne[j];
+                    moyenne[j] = echangeMoyenne;
+
+                    double[] echangeNote = note[i];
+                    note[i] = note[j];
+                    note[j] = echangeNote;
+
+                    double[] echangeCoef = coef[i];
+                    coef[i] = coef[j];
+                    coef[j] = echangeCoef;
+                }
+            }
+        }
+
+
+        System.out.println("Voici le classement des élève de la meilleure à la moins bonne moyenne :");
+        for (int i = 0; i < nombreEleve; i++) {
+            System.out.println((i + 1) + ". " + nomEtudiant[i] + " : " + moyenne[i]);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 }
